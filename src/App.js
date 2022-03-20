55b0c47a9f27545a4733d3a2dc4f508858af4e9c
Tohidul Islam <mdtohid.sust@gmail.com>
2022-03-21 00:20:55 +0600

    context version initiated in branch-Context_version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { UserProvider } from "../../Contexts/HeaderContext.js";
 
 function App() {
+  console.log("App rendered");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <div>App</div>
+    </UserProvider>
   );
 }
 
---
+++ src/Contexts/TodosContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useReducer, useEffect } from "react";
+
+let Context = null;
+const { Provider, Consumer } = (Context = createContext());
+
+const initialState = [];
+const reducer = (state, { type, value }) => {
+  switch (type) {
+    case "addTodo":
+      return [...state, { value }];
+    case "deleteTodo":
+      return value;
+    default:
+      return state;
+  }
+};
+
+function UserProvider(props) {
+  const [data, dispatch] = useReducer(reducer, initialState);
+  return (
+    <Provider
+      value={{
+        ...data,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </Provider>
+  );
+}
+export { UserProvider, Consumer as UserConsumer, Context as UserContext };
